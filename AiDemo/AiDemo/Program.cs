8840f11994ff71fbebd30ae09993f2efe0f39750
Devin Tullier <devin@elevator3.com>
2020-09-11 15:04:10 -0500

    wip: started eight puzzle

---
+++ AiDemo/AiDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EightPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PromptForAlgorithm();
+        }
+
+        public static void PromptForAlgorithm()
+        {
+            Console.WriteLine("Which algorithm would you like to use?");
+            var algs = new List<string> { "BFS", "DFS", "A*" };
+            for(var i = 0; i < algs.Count; i++)
+            {
+                Console.WriteLine($"{i}: {algs[i]}");
+            }
+            var alg = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (alg)
+            {
+                case '0':
+                    Console.WriteLine(algs[0]);
+                    BfSearch();
+                    break;
+                case '1':
+                    Console.WriteLine(algs[1]);
+                    break;
+                case '2':
+                    Console.WriteLine(algs[2]);
+                    break;
+                default:
+                    Console.WriteLine("You broke it...");
+                    break;
+            }
+        }
+
+        public static void BfSearch()
+        {
+
+        }
+    }
+}
